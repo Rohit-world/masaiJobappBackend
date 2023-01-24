130a32834e27918c526b8c7b6823bafebbab098a
xxRohitxx <rohitrs319791@gmail.com>
2023-01-24 13:21:41 +0530

    job route

--- routes/job.route.js
+++ routes/job.route.js
@@ -2,10 +2,63 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const JobRoute = express.Router();
 const { JobModel } = require("../models/job.model");
-JobRoute.post("/", async (req, res) => {
 
+JobRoute.get("/", async(req,res)=>{
+ const AllUsers= await JobModel.find()
+ res.send(AllUsers)
+})
+
+
+JobRoute.post("/", async (req, res) => {
+ const {Company_name,
+  Position,
+  Contract,
+  Location}=req.body
+  const NewJob= new JobModel({Company_name,Position,Contract,Location})
+  await NewJob.save()
+  res.send({"msg":"Job added sucessfully"})
   });
+
+
+  JobRoute.patch("/:ID", async (req, res) => {
+const {ID}=req.params
+    const {Company_name,
+     Position,
+     Contract,
+     Location}=req.body
+
+     JobModel.findByIdAndUpdate(ID, { Company_name,
+      Position,
+      Contract,
+      Location},
+     function (err, docs) {
+if (err){
+  res.send({"msg":"Error in Updating file"})
+}
+else{
+  res.send({"msg":"Job Updated Successfull sucessfull"})
+}
+})
+      
+     });
   
+
+
+     JobRoute.delete("/:ID", async (req, res) => {
+      const {ID}=req.params
+     
+      
+           JobModel.findByIdAndDelete(ID,
+           function (err, docs) {
+      if (err){
+        res.send({"msg":"Error in Deleting file"})
+      }
+      else{
+        res.send({"msg":"Job Deleted Successfull sucessfull"})
+      }
+      })
+            
+           });
 
 
 module.exports = { JobRoute };
